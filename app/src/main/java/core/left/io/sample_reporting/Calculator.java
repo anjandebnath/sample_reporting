36afd99412ec2eaf128c81be814e94876ef862de
Azim <ahmedmohmmadullah@gmail.com>
2018-12-24 13:34:05 +0600

    initial commit

---
+++ app/src/main/java/core/left/io/sample_reporting/Calculator.java
@@ -0,0 +1,8 @@
+package core.left.io.sample_reporting;
+
+public class Calculator {
+
+    public double add(double num1,double num2){
+        return num1 + num2;
+    }
+}
---
+++ app/src/main/java/core/left/io/sample_reporting/MainActivity.java
@@ -0,0 +1,32 @@
+package core.left.io.sample_reporting;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+
+import core.left.io.sample_reporting.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        final Calculator calculator = new Calculator();
+
+        binding.btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!TextUtils.isEmpty(binding.etNum1.getText())){
+                    binding.tvHasil.setText(String.valueOf(
+                            calculator.add(
+                                    Double.parseDouble(binding.etNum1.getText().toString()),
+                                    Double.parseDouble(binding.etNum2.getText().toString()))));
+                }
+            }
+        });
+    }
+}
